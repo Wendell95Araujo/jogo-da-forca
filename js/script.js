6454de9d8998c4c24443bf4136b2916257be829e
Wendell Araujo <wendell170695@gmail.com>
2024-12-09 16:09:14 -0300

    novo layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 const db = firebase.database();
 
 // Seletores jQuery
-const $categoriaSelect = $("#categoria");
 const $iniciarJogoBtn = $("#iniciarJogo");
+const $reiniciarJogoBtn = $("#reiniciarJogo");
 const $palavraDiv = $("#palavra");
 const $tecladoDiv = $("#teclado");
 const $dicaDiv = $("#dica");
+const $configButton = $("#configuracoes");
+const $pontuacoesDiv = $("#Pontuações");
+const $pontuacao = $("#pontuacao");
+const $record = $("#record");
 
 // Partes do boneco
 const partesBoneco = [
@@ -18,6 +22,8 @@ const partesBoneco = [
 ];
 
 // Variáveis do jogo
+let categoriasList = [];
+let categoriaSelect = localStorage.getItem("categoriaSelect") || "todas";
 let palavraAtual = "";
 let erros = 0;
 let pontuacao = 0;
@@ -29,10 +35,9 @@ function carregarCategorias() {
   categoriasRef.once("value").then((snapshot) => {
     if (snapshot.exists()) {
       const categorias = snapshot.val();
-      $categoriaSelect.empty().append("<option value='todas'>Todas</option>");
       $.each(categorias, (categoria, palavras) => {
-        $categoriaSelect.append(
-          `<option value="${categoria}">${categoria}</option>`
+        categoriasList.push(
+          `<option value="${categoria}">${categoria.toUpperCase()}</option>`
         );
       });
     }
@@ -41,7 +46,12 @@ function carregarCategorias() {
 
 // Função para iniciar o jogo
 function iniciarJogo() {
-  const categoria = $categoriaSelect.val();
+  $pontuacoesDiv.css("display", "flex");
+  $pontuacao.text(`Pontuação: ${pontuacao}`);
+  $record.text(`Recorde: ${pontuacaoMax}`);
+  $iniciarJogoBtn.hide();
+  $reiniciarJogoBtn.css("display", "flex");
+  const categoria = categoriaSelect || "todas";
   let categoriaText = "";
 
   const palavrasRef = db.ref(
@@ -71,8 +81,12 @@ function iniciarJogo() {
       sessionStorage.setItem("palavrasUsadas", JSON.stringify(usadas));
 
       if (categoria === "todas") {
-        const categoriaPalavra = Object.keys(snapshot.val()).find(cat => snapshot.val()[cat].includes(palavraAtual));
-        categoriaText = categoriaPalavra ? categoriaPalavra : "Categoria desconhecida";
+        const categoriaPalavra = Object.keys(snapshot.val()).find((cat) =>
+          snapshot.val()[cat].includes(palavraAtual)
+        );
+        categoriaText = categoriaPalavra
+          ? categoriaPalavra
+          : "Categoria desconhecida";
       } else {
         categoriaText = categoria;
       }
@@ -98,7 +112,7 @@ function mostrarPalavra() {
   $palavraDiv.text(palavraFormatada);
 }
 
-function verificarLetra(letra, $botao) {
+function verificarLetra(letra) {
   if (!letra) return;
 
   const letraNormalizada = normalizarString(letra.toUpperCase());
@@ -108,33 +122,61 @@ function verificarLetra(letra, $botao) {
   const letrasNaPalavra = Array.from(palavraNormalizada);
   const letrasNaPalavraPadrao = Array.from(palavraAtual.toUpperCase());
 
-  $botao.prop("disabled", true);
+  $(`.letra${letra}`).prop("disabled", true);
 
   if (letrasNaPalavra.includes(letraNormalizada)) {
     letrasNaPalavra.forEach((l, i) => {
       if (l === letra) letrasExibidas[i] = letrasNaPalavraPadrao[i];
     });
-    $botao.addClass("correta");
+    $(`.letra${letra}`).addClass("correta");
     $palavraDiv.text(letrasExibidas.join(""));
 
     if (!letrasExibidas.includes("_")) {
-      alert("Parabéns, você venceu!");
-      pontuacao++;
-      if (pontuacao > pontuacaoMax) {
-        pontuacaoMax = pontuacao;
-        localStorage.setItem("pontuacaoMax", pontuacaoMax);
-      }
-      iniciarJogo();
+      Swal.fire({
+        toast: true,
+        position: "center",
+        showConfirmButton: true,
+        allowEscapeKey: false,
+        confirmButtonText: "Continuar",
+        customClass: {
+          confirmButton: "btn btn-primary",
+        },
+        title: `<i class="fa-solid fa-check"></i> Parabéns, você venceu!`,
+      }).then((result) => {
+        if (!result.isConfirmed) return;
+        pontuacao++;
+        if (pontuacao > pontuacaoMax) {
+          pontuacaoMax = pontuacao;
+          $record.text(`Recorde: ${pontuacaoMax}`);
+        }
+        $pontuacao.text(`Pontuação: ${pontuacao}`);
+        iniciarJogo();
+      });
     }
   } else {
     erros++;
-    $botao.addClass("errada");
+    $(`.letra${letra}`).addClass("errada");
     if (erros < partesBoneco.length) {
       partesBoneco[erros - 1].show();
     } else {
-      alert(`Você perdeu! A palavra era: ${palavraAtual}`);
-      pontuacao = 0;
-      iniciarJogo();
+      swal
+        .fire({
+          toast: true,
+          position: "center",
+          showConfirmButton: true,
+          allowEscapeKey: false,
+          confirmButtonText: "Novo Jogo",
+          customClass: {
+            confirmButton: "btn btn-primary",
+          },
+          title: `<i class="fa-solid fa-xmark"></i> Que pena, você perdeu!`,
+          text: `A palavra era: ${palavraAtual.toUpperCase()}`,
+        })
+        .then((result) => {
+          if (!result.isConfirmed) return;
+          pontuacao = 0;
+          iniciarJogo();
+        });
     }
   }
 }
@@ -153,14 +195,100 @@ function normalizarString(str) {
 // Função para criar o teclado
 function criarTeclado() {
   $tecladoDiv.empty();
+
+  const $teclado = $(`<div id="tecladoCompleto" class="teclado-completo"></div>`);
   "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").forEach((letra) => {
-    const $botao = $(`<button>${letra}</button>`);
-    $botao.on("click", () => verificarLetra(letra, $botao));
-    $tecladoDiv.append($botao);
+    const $botao = $(`<button class="letra${letra}">${letra}</button>`);
+    $botao.on("click", () => verificarLetra(letra));
+    $teclado.append($botao);
   });
+
+  const $tecladoDiv1 = $(`<div id="teclado-linha1" class="teclado-linha"></div>`);
+  "QWERTYUIOP".split("").forEach((letra) => {
+    const $botao = $(`<button class="letra${letra}">${letra}</button>`);
+    $botao.on("click", () => verificarLetra(letra));
+    $tecladoDiv1.append($botao);
+  });
+
+    const $tecladoDiv2 = $(`<div id="teclado-linha2" class="teclado-linha"></div>`);
+    "ASDFGHJKL".split("").forEach((letra) => {
+      const $botao = $(`<button class="letra${letra}">${letra}</button>`);
+      $botao.on("click", () => verificarLetra(letra));
+      $tecladoDiv2.append($botao);
+    });
+
+    const $tecladoDiv3 = $(`<div id="teclado-linha3" class="teclado-linha"></div>`);
+    "ZXCVBNM".split("").forEach((letra) => {
+      const $botao = $(`<button class="letra${letra}">${letra}</button>`);
+      $botao.on("click", () => verificarLetra(letra));
+      $tecladoDiv3.append($botao);
+    });
+
+    $tecladoDiv.append($teclado, $tecladoDiv1, $tecladoDiv2, $tecladoDiv3);
 }
 
 // Evento de clique para iniciar o jogo
 $iniciarJogoBtn.on("click", iniciarJogo);
+
+// Evento de clique para reiniciar o jogo
+$reiniciarJogoBtn.on("click", () => {
+  if (pontuacao === 0) {
+    iniciarJogo();
+  } else {
+    swal.fire({
+        toast: true,
+        position: "center",
+        showConfirmButton: true,
+        allowEscapeKey: false,
+        confirmButtonText: "Novo Jogo",
+        showCancelButton: true,
+        cancelButtonText: "Cancelar",
+        customClass: {
+          confirmButton: "btn btn-primary",
+          cancelButton: "btn btn-secondary",
+        },
+        title: `<i class="fa-solid fa-rotate-right"></i> Reiniciar Jogo?`,
+        text: `Sua pontuação atual é ${pontuacao} ${pontuacao === 1 ? "ponto" : "pontos"}. Tem certeza que deseja reiniciar o jogo? Você perderá essa pontuação.`,
+      })
+      .then((result) => {
+        if (!result.isConfirmed) return;
+        pontuacao = 0;
+        iniciarJogo();
+      });
+  }
+});
+
+// Evento de clique alterar configurações
+$configButton.on("click", () => {
+  swal.fire({
+    title: `<i class="fa-solid fa-cog"></i> Configurações`,
+    html: `
+    <div class="container-form">
+      <label for="categoria" class="form-label">Selecione uma categoria:</label>
+            <select id="categoria" class="swal2-input">
+                <option value='todas'>TODAS</option>
+                ${categoriasList.join("")}
+            </select>
+    </div>
+    `,
+    showCancelButton: true,
+    confirmButtonText: "Salvar",
+    showLoaderOnConfirm: true,
+    customClass: {
+      confirmButton: "btn btn-primary",
+      cancelButton: "btn btn-secondary",
+      title: "swal2-title",
+    },
+    didOpen: () => {
+      $("#categoria").val(categoriaSelect);
+    },
+    preConfirm: () => {
+      const categoria = $("#categoria").val();
+      localStorage.setItem("categoriaSelect", categoria);
+      categoriaSelect = categoria;
+      carregarCategorias();
+    },
+  });
+});
 
 carregarCategorias();
